c43b256f72531577fcfe3fdbd9a32f59f1227608
Death-Momoi <aib1343040@gmail.com>
2026-09-23 23:45:41 +0800

    Delete migrations/20260923000500_grant_profile_access_to_service_role.sql

--- migrations/20260923000500_grant_profile_access_to_service_role.sql
+++
@@ -1,5 +0,0 @@
--- Edge Functions using the server-only secret key need profile names when
--- composing issue notification emails. This does not grant browser access.
-grant select
-on table public.profiles
-to service_role;
